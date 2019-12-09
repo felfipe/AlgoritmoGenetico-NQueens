ee9e4145238a278663288181852bc5aa66d28e05
Victor Amaral <victoramaral@usp.br>
2019-12-09 03:55:08 -0300

    Base do jogo criada

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-void pecas(); //print da tabela de peças
-void receberPecas(int *pontVec, int qnt);
-
-void atribuirPos(int *matriz); 
-
-//void dama(int posX, int posY, int n, int *tab);
-void lateral(int posX, int posY, int n, int *tab);
-void diagonal(int posX, int posY, int n, int *tab);
+#include <stdlib.h>
+int** cria_tabuleiro(int n);
+int insere_peca(int** tabuleiro, int posX, int posY, int tipo_peca);
+int verifica_ataque(int** tabuleiro, int n, int posX, int posY);
+void apaga_tabuleiro(int **tabuleiro, int n);
+void limpa_tabuleiro(int **tabuleiro, int n);
+void printa_tabuleiro(int **tabuleiro, int n);
 //Em tab 0 indica que não há peças no local e 1 indica que há peças
 
 int main()
@@ -15,168 +13,149 @@ int main()
     int n; //tamanho do tabuleiro 
     int qnt; //quantidade de peças
     int *pointer; //transmite os vetores como paramêtros
-
-    printf("Sizeof(int)= %d\n", sizeof(int));
     printf("Digite a dimensao n desejada para o tabuleiro: ");
     scanf("%d", &n);
-    printf("Digite a quantidade de peças que deseja no tabuleiro: ");
-    scanf("%d", &qnt);
-
-    int *tabuleiro = (int *)malloc(n * n * sizeof(int));
-    //tabuleiro NxN, n é entrada
-    int *vetorPec = (int *)malloc(qnt * 3 * sizeof(int)); 
-    //matriz com a identificação da peça e posição dela no tabuleiro
-    
-    
-    printf("Digite quais pecas deseja no tabuleiro %dx%d\nsao %d pecas\n", n, n,qnt);
-    pecas(); //printa a tabela de referencia de pecas
-    receberPecas(vetorPec, qnt); //função para definir as peças
-
-    printf("\n\n");
-    //printf("Gerando posições iniciais...");
-    int j;
-    for(j=0; j<qnt; j++){
-      printf("%d, ", *(vetorPec+4*3*j));
+    int **tabuleiro = cria_tabuleiro(n);
+    if(tabuleiro == NULL){
+        printf("\nErro, tabuleiro não alocado.");
+        return 0;
     }
-
-    //dama(3,4,n, tabuleiro);
-
-
+    limpa_tabuleiro(tabuleiro, n);
+    for(int i = 0; i < n; i++){
+      int posX, posY;
+      scanf("%d %d", &posX, &posY);
+      insere_peca(tabuleiro,posX,posY,1);
+    }
+    printa_tabuleiro(tabuleiro, n);
+    int ataque = verifica_ataque(tabuleiro, n, 0, 1);
+    printf("\nataque: %d",ataque);
     free(tabuleiro);
-    free(vetorPec);
     printf("\nFim");
     return 0;
 }
 
-void pecas(){ //print da tabela de peças
-
-  printf("1. Dama\n2. Torre\n3. Bispo\n");
-
-}
-
-void receberPecas(int *pontVec, int qnt){
-
-    int i, var;
-    printf("%d\n", pontVec);
-    for(i=0; i<qnt; i++){ //fazer percorrer o sempre na primeira linha
-      scanf("%d", &var);//adiciona a peça
-      if(var>0 && var<4){ //de 1 a 3
-        *(pontVec+sizeof(int)*3*i) = var;  //4bytes, 3 colunas
-      }
-      else {
-        *(pontVec+sizeof(int)*3*i) = 1; //por default se alguem digitar errado é dama
-      }
-      printf("Peca %d = %d\n\n", i+1, *(pontVec+sizeof(int)*3*i));
-      printf("Endereço = %d\n\n", (pontVec+sizeof(int)*3*i));
-    }
-
-}
-
-void atribuirPos(int *matriz){
-
-}
-
-//*******FUNÇÕES ANDAR LATERAL E DIAGONAL, NO CASE DA DAMA CHAMAR OS 2*******//
 
 /*
-void dama(int posX, int posY, int n, int *tab){
-    int x,y;
-    int cont;
-
-    for(x=0; x<n; x++){ 
-      //movimento na coluna
-        if(*(tab+sizeof(int)*(n*x + posY)) == 1){ //Se tiver uma peça
-          *(tab+sizeof(int)*(n*x + posY)) = 0;    //a peça é comida 
-          //Só some no tabuleiro
-          //no vetor de peças se mantém
-        }
-    }
-    for(y=0; y<n; y++){ 
-      //movimento na linha
-        if(*(tab+sizeof(int)*(n*posX + y)) == 1){
-          *(tab+sizeof(int)*(n*posX + y)) = 0;  
-      }
-    }
-
-    
-    for(cont=0; posX+cont<n && posY+cont<n; cont++){
-      //"primeiro quadrante"
-        if(*(tab+sizeof(int)*(n*(posX+cont)+(posY+cont))) == 1 ){
-          *(tab+sizeof(int)*(n*(posX+cont)+(posY+cont))) = 0;
-        }
-    }
-    for(cont=0 ; posX-cont>=0 && posY+cont<n; cont++){
-      //desce em x e sobe em y
-      //"Segundo quadrante"
-        if(*(tab+sizeof(int)*(n*(posX-cont)+(posY+cont))) == 1 ){
-          *(tab+sizeof(int)*(n*(posX-cont)+(posY+cont)))=0;
-        }
-    }
-    for(cont=0 ; posX-cont>=0 && posY-cont>=0; cont++){
-      //"Terceiro quadrante"
-        if(*(tab+sizeof(int)*(n*(posX-cont)+(posY-cont))) == 1 ){
-          *(tab+sizeof(int)*(n*(posX-cont)+(posY-cont)))=0;
-        }
-    }
-    for(cont=0 ; posX+cont<n && posY-cont>=0; cont++){ 
-      //sobe em x e desce em y
-      //"Quarto quadrante"
-        if(*(tab+sizeof(int)*(n*(posX+cont)+(posY-cont))) == 1 ){
-          *(tab+sizeof(int)*(n*(posX+cont)+(posY-cont)))=0;
-        }
-    }
-
-}
+Função que aloca o tabuleiro do jogo
+retorna:
+end. do tabuleiro -> SUCESSO
+-1 -> ERRO
 */
-
-
-
-void lateral(int posX, int posY, int n, int *tab){
-    int x,y;
-    int cont;
-
-    for(x=0; x<n; x++){ 
-      //movimento na coluna
-        if(*(tab+sizeof(int)*(n*x + posY)) == 1){ //Se tiver uma peça
-          *(tab+sizeof(int)*(n*x + posY)) = 0;    //a peça é comida 
-          //Só some no tabuleiro
-          //no vetor de peças se mantém
-        }
+int** cria_tabuleiro(int n){
+    int **tabuleiro = calloc(n,sizeof(int*));
+    if(tabuleiro == NULL)
+        return NULL;
+    for(int i = 0; i < n;i++){
+        tabuleiro[i] = calloc(n,sizeof(int));
+        if(tabuleiro[i] == NULL)
+            return NULL;
     }
-    for(y=0; y<n; y++){ 
-      //movimento na linha
-        if(*(tab+sizeof(int)*(n*posX + y)) == 1){
-          *(tab+sizeof(int)*(n*posX + y)) = 0;  
-      }
-    }
+    return tabuleiro;
 }
-void diagonal(int posX, int posY, int n, int *tab){
-    int x,y, cont;
 
-    for(cont=0; posX+cont<n && posY+cont<n; cont++){
-      //"primeiro quadrante"
-        if(*(tab+sizeof(int)*(n*(posX+cont)+(posY+cont))) == 1 ){
-          *(tab+sizeof(int)*(n*(posX+cont)+(posY+cont))) = 0;
+
+/*
+Apaga o tabuleiro nxn
+*/
+void apaga_tabuleiro(int **tabuleiro, int n){
+  for(int i = 0; i < n; i++)
+    free(tabuleiro[i]);
+    free(tabuleiro);
+}
+
+
+/*
+Reinicializa o tabuleiro (seta todos os campos = 0)
+*/
+void limpa_tabuleiro(int** tabuleiro, int n){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++)
+      tabuleiro[i][j] = 0;
+  }
+}
+
+
+/*
+Insere uma peça no tabuleiro
+Retorno:
+-1 - ERRO
+1 - SUCESSO
+*/
+int insere_peca(int** tabuleiro, int posX, int posY, int tipo_peca){
+    if(tabuleiro[posX][posY] != 0)
+        return 0;
+    tabuleiro[posX][posY] = tipo_peca;
+    return 1;
+}
+
+
+/*
+Verifica a quantidade de ataques que uma peça pode realizar.
+retorna:
+-1 -> ERRO
+num ataques -> SUCESSO
+*/
+int verifica_ataque(int** tabuleiro, int n, int posX, int posY){
+    int ataques = 0;
+    if(posX >= n|| posY >= n)
+        return -1;
+    int tipo = tabuleiro[posX][posY];
+    if(tipo == 1|| tipo == 2){
+        for(int i = 0; i < n; i++){
+            if(i == posY)
+              continue;
+            if(tabuleiro[posX][i] != 0){
+                ataques++;
+                break;
+            }                
+        }
+        for(int i = 0; i < n; i++){
+            if(i == posX)
+                continue;
+            if(tabuleiro[i][posY] != 0){
+                ataques++;
+                break;
+            }                
         }
     }
-    for(cont=0 ; posX-cont>=0 && posY+cont<n; cont++){
-      //desce em x e sobe em y
-      //"Segundo quadrante"
-        if(*(tab+sizeof(int)*(n*(posX-cont)+(posY+cont))) == 1 ){
-          *(tab+sizeof(int)*(n*(posX-cont)+(posY+cont)))=0;
+    if(tipo == 1|| tipo == 3){
+        for(int i = 1; (posX+i<n)&&(posY+i<n); i++){
+            if(tabuleiro[posX+i][posY+i] != 0){
+                ataques++;
+                break;
+            }                
+        }
+        for(int i = 1; (posX-i>=0)&&(posY-i>=0); i--){
+            if(tabuleiro[posX-i][posY-i] != 0){
+                ataques++;
+                break;
+            }                
+        }
+        for(int i = 1; (posX+i<n)&&(posY-i>=0); i++){
+            if(tabuleiro[posX+i][posY-i] != 0){
+                ataques++;
+                break;
+            }                
+        }
+        for(int i = 1; (posX-i>=0)&&(posY+i<n); i++){
+            if(tabuleiro[posX-i][posY+i] != 0){
+                ataques++;
+                break;
+            }                
         }
     }
-    for(cont=0 ; posX-cont>=0 && posY-cont>=0; cont++){
-      //"Terceiro quadrante"
-        if(*(tab+sizeof(int)*(n*(posX-cont)+(posY-cont))) == 1 ){
-          *(tab+sizeof(int)*(n*(posX-cont)+(posY-cont)))=0;
-        }
+    return ataques;
+}
+
+
+/*
+Printa o tabuleiro na tela do console
+*/
+void printa_tabuleiro(int **tabuleiro, int  n){
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j< n; j++)
+        printf(" %d ", tabuleiro[i][j]);
+      printf("\n");
     }
-    for(cont=0 ; posX+cont<n && posY-cont>=0; cont++){ 
-      //sobe em x e desce em y
-      //"Quarto quadrante"
-        if(*(tab+sizeof(int)*(n*(posX+cont)+(posY-cont))) == 1 ){
-          *(tab+sizeof(int)*(n*(posX+cont)+(posY-cont)))=0;
-        }
-    }
+
 }
