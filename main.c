594a00701002090eaf20d06a482af139d8e7fedd
Victor Amaral <victoramaral@usp.br>
2019-12-12 21:56:10 -0300

    AG funcionando com elitismo e torneio de 2

--- main.c
+++ main.c
@@ -19,10 +19,13 @@ void preenche_aleatorio(int* vetor, int n);
 int* gera_vetor_fitness(int** population, int** tabuleiro, int n);
 int avalia_individuo(int **tabuleiroint, int* individuo, int n);
 int recombina_individuos_elitismo(int** population, int n, int* fitness);
-int mutacao(int** population, int n);
+int recombina_individuos_torneio(int*** population, int n, int* fitness);
+int escolhe_pai(int* fitness);
+int mutacao(int** population, int n, int taxa_mutacao);
 
-#define MAX_POPULATION 20
+#define MAX_POPULATION 50
 #define MUTATION_RATE  1
+#define PROB_PAI 0.5     // PROBABILIDADE DE SELECIONAR O MELHOR INDIVIDUO PARA SER PAI NO TORNEIO DE 2
 
 
 int main()
@@ -41,6 +44,8 @@ int main()
     int criterio_parada = 1;
     int melhor_individuo[n];
     int geracao = 0;
+    int taxa_mutacao = 1;
+    int fitness_anterior = 1000000;
     while(criterio_parada){
       int **populacao = inicializa_populacao(n);
       if(geracao > 0)
@@ -49,10 +54,11 @@ int main()
             populacao[0][i] = melhor_individuo[i];
       while(1){
         int* fitness = gera_vetor_fitness(populacao,tabuleiro,n);
+        //int index_melhor_individuo = recombina_individuos_torneio(&populacao,n,fitness);    
         int index_melhor_individuo = recombina_individuos_elitismo(populacao,n,fitness);
         for(int i = 0; i < n; i++)
           melhor_individuo[i] = populacao[index_melhor_individuo][i];
-        mutacao(populacao,n);
+        mutacao(populacao,n, taxa_mutacao);
         limpa_tabuleiro(tabuleiro, n);
         insere_individuo(populacao[index_melhor_individuo], tabuleiro, n);
         //printa_tabuleiro(tabuleiro, n);
@@ -70,20 +76,27 @@ int main()
           criterio_parada = 0;
           break;
         }
-        if(geracao == 50000){
+        if(fitness_melhor_individuo == fitness_anterior)
+          taxa_mutacao++;
+        else if(fitness_melhor_individuo < fitness_anterior)
+          taxa_mutacao = 0;
+        fitness_anterior = fitness_melhor_individuo;
+        if(taxa_mutacao == n){
           genocidios++;
-          geracao = 1;
+          taxa_mutacao = 1;
           apaga_populacao(populacao);
           break;
         }
       }
     }
-
-
     free(tabuleiro);
     printf("\nFim");
     return 0;
 }
+
+
+
+
 /*
 Gera vetor de fitness normalizado da população.
 100 -> melhor
@@ -98,6 +111,51 @@ int* gera_vetor_fitness(int** population, int** tabuleiro, int n){
 }
 
 
+
+/*
+Seleciona os indivíduos para nova população com base no método de torneio de 2.
+retorna o index do melhor indivíduo.
+*/
+int recombina_individuos_torneio(int*** population, int n, int *fitness){
+  int index_melhorIndividuo;
+  int fitness_melhorIndividuo;
+  for(int i = 0; i< MAX_POPULATION; i++)
+      if(fitness[i] < fitness_melhorIndividuo){
+        index_melhorIndividuo = i;
+        fitness_melhorIndividuo = fitness[i];
+      }
+  int** new_population = inicializa_populacao(n);
+  for(int i = 0; i < n; i++)
+    new_population[0][i] = (*population)[index_melhorIndividuo][i];
+
+  for(int i = 1; i < MAX_POPULATION; i++){
+    int pai1 = escolhe_pai(fitness);
+    int pai2 = escolhe_pai(fitness);
+    int corte = rand()%n;
+    for(int j = 0; j < corte; j++)
+      new_population[i][j] = (*population)[pai1][j];
+    for(int j = corte; j < n; j++)
+      new_population[i][j] = (*population)[pai2][j];
+    apaga_populacao(*population);
+    *population = new_population;
+    return 0;
+  }
+
+
+}
+/*
+Função que escolhe aleatoriamente 2 indivíduos e seleciona o melhor para ser pai.
+*/
+int escolhe_pai(int* fitness){
+  int individuo1 = rand()%MAX_POPULATION;
+  int individuo2 = rand()%MAX_POPULATION;
+  float num = rand()%1000/1000.0;
+  if(num < PROB_PAI)
+    return fitness[individuo1]>fitness[individuo2]?individuo1:individuo2;
+  else
+    return fitness[individuo1]<fitness[individuo2]?individuo1:individuo2;
+  
+}
 /*
 Função que faz a recombinação dos indivíduos.
 O método utilizado é pegar o melhor indivíduo e recombinar com toda a população.
@@ -140,18 +198,20 @@ int seleciona_melhor_individuo(int *fitness){
 Passa por todos os indivíduos e calcula a probabilidade de mutação.
 Caso ocorra, sorteia um genoma entre 0 e n e soma ou subtrai 1 deste aleatoriamente.
 */
-int mutacao(int **population, int n){
+int mutacao(int **population, int n, int taxa_mutacao){
   for(int i = 0; i < MAX_POPULATION; i++){
       if((rand()%1000/1000.0) <= MUTATION_RATE){
-        int genoma_mutado = rand()%n;
-        if(population[i][genoma_mutado] == 0)
-          population[i][genoma_mutado] +=1;
-        else if(population[i][genoma_mutado] == n-1)
-          population[i][genoma_mutado] -=1;
-        else if(rand()%2)
-          population[i][genoma_mutado] +=1;
-        else
-          population[i][genoma_mutado] -=1;
+        for(int j = 0; j < taxa_mutacao; j++){
+          int genoma_mutado = rand()%n;
+          if(population[i][genoma_mutado] == 0)
+            population[i][genoma_mutado] +=1;
+          else if(population[i][genoma_mutado] == n-1)
+            population[i][genoma_mutado] -=1;
+          else if(rand()%2)
+            population[i][genoma_mutado] +=1;
+          else
+            population[i][genoma_mutado] -=1;
+        }
       }
   }
   return 1;
